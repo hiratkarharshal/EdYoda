3bd2a64676928acd5f6e7e4b7e29e8d140c29fbe
Harshal-simplecrm <harshal@simplecrm.com>
2022-04-18 17:25:48 +0530

    added final certification project of IPL web application

--- Javascript/practice/index.js
+++ Javascript/practice/index.js
@@ -1,24 +1,36 @@
-var person = "micheal"
+const mFunc = (arg) =>{
 
-var people = [["mike", "paul"],
-["tom", "robin"],
-["jae", "paul"],
-["paul", "micheal"],
-["robin", "ross"]]
+if (arg != undefined && arg !== null) {
+     return Object.keys (arg).map((objKey) => {
+          return (
+            [...Array(arg[objKey])].map((orderItem, key) => {
 
-for (var i = 0; i < people.length; i++) {
+                return ({
 
-      if (people[i][0] == person) {
-            findg(people[i][1]);
-      }
+                    id: arg[objKey].id,
+
+                    name: arg[objKey].name
+
+                });
+
+            })
+          )
+}).reduce((acc, item) => { return acc.concat(item)
+
+}, []);
+
+} else {
+
+return []
+
 }
-
-function findg(father) {
-      for (var j = 0; j < people.length; j++) {
-
-            if (people[j][0] == father) {
-                  console.log(people[j][1]);
-            }
-
-      }
-}     
+}
+var objCus = [{
+    "name":"harshal",
+    "age":24
+},
+{
+    "name":"harshal",
+    "age":24
+}]
+console.log(mFunc(objCus));
